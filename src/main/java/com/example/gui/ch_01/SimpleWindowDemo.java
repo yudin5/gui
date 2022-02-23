d7306dde5a9197444e4dff083a6721f894bcc75b
yudin5 <yudin5@yandex.ru>
2022-02-23 14:50:40 +0300

    add one more simple example

--- src/main/java/com/example/gui/ch_01/SimpleWindowDemo.java
+++ src/main/java/com/example/gui/ch_01/SimpleWindowDemo.java
@@ -6,5 +6,11 @@ public class SimpleWindowDemo {
     public static void main(String[] args) {
         // Отображение информационного окна
         JOptionPane.showMessageDialog(null, "Всем привет!");
+        JOptionPane.showMessageDialog(null, "Всем пока!");
+//        System.getProperties().forEach((k, v) -> {
+//            System.out.printf("%s = %s%n", k, v);
+//        });
+//        String property = System.getProperty("java.class.path");
+//        JOptionPane.showMessageDialog(null, property);
     }
 }
